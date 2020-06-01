b9996c5f8b4fb2133ec64efec6743b788242839d
michaelcurrin <michael.currin@gmail.com>
2020-06-01 22:51:36 +0200

    Update example2.py

--- recipes/python/templating/using-genshi/example2.py
+++ recipes/python/templating/using-genshi/example2.py
@@ -14,9 +14,7 @@ tpl_xml = """
 
 tpl = MarkupTemplate(tpl_xml)
 
-data = [{'a': 1, 'b': 2},
-        {'c': 3, 'd': 4}]
-stream = tpl.generate(data=[1, 2, 3, 4]
+stream = tpl.generate(data=[1, 2, 3, 4])
 
 with open('output2.xml', 'w') as f:
     stream.render(out=f)
